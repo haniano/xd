dcfea98828983db7835c09bf1750fafaa4402632
Ryszard Pulawski <ryszap@gmail.com>
2021-11-26 20:06:25 +0100

    first class architecture, no impl, no strategy

--- src/Board.java
+++ src/Board.java
@@ -1,2 +1,16 @@
-public class Board {
+import java.util.ArrayList;
+
+public abstract class Board {
+    ArrayList<ArrayList<Spot>> board;
+    abstract void setupBoard();
+    Spot getSpot(int i, int j){
+        return board.get(i).get(j);
+    }
+}
+
+
+class ChessBoard extends Board{
+    @Override
+    void setupBoard() {
+    }
 }
