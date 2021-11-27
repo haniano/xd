9f316e14742aa452ba89796923358e02b58cdb8f
Ryszard Pulawski <ryszap@gmail.com>
2021-11-27 11:19:58 +0100

    Board + Spot architecture + ugly Pair impl

--- src/Spot.java
+++ src/Spot.java
@@ -2,8 +2,26 @@
 
 public abstract class Spot {
     Pair<Integer, Integer> coord;
+    Piece piece;
+    // lets assume white is 0, and black is 1
+    boolean color;
+
+    public Spot(Integer i, Integer j, Piece piece, boolean color){
+        this.coord = new Pair<>(i, j);
+        this.color = color;
+        this.piece = piece;
+    }
+    public Spot getSpot(){ return this; }
+    public void setSpot(Spot spot){
+        this.coord = spot.coord;
+        this.piece = spot.piece;
+        this.color = spot.color;
+    }
 }
 
 class ChessSpot extends Spot {
 
+    public ChessSpot(Integer i, Integer j, Piece piece, boolean color) {
+        super(i, j, piece, color);
+    }
 }
