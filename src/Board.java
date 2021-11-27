bbea41ae1b2df63167db3aaffbced379774c8ef7
Ryszard Pulawski <ryszap@gmail.com>
2021-11-27 12:19:14 +0100

    Board + Spot architecture + ugly Pair impl

--- src/Board.java
+++ src/Board.java
@@ -2,14 +2,31 @@ import java.util.ArrayList;
 
 public abstract class Board {
     ArrayList<ArrayList<Spot>> board;
-    abstract void setupBoard();
-    Spot getSpot(int i, int j){
+    abstract public void setupBoard();
+    public Spot getSpot(int i, int j){
         return board.get(i).get(j);
     }
 }
 
 class ChessBoard extends Board{
     @Override
-    void setupBoard() {
+    public void setupBoard() {
+        for(int i = 0; i<8; i++){
+            for(int j=0; j<8; j++)
+                board.get(i).get(j).setSpot(getCorrectSpotInit(i, j));
+        }
+    }
+
+// helpers
+    Spot getCorrectSpotInit(int i, int j){
+        boolean color = getCorrectColor(i, j);
+        Spot spot = new ChessSpot(i, j, color);
+        // TODO: madrze zakodzic to, jaka figurke zwraca, czy zostaje null
+        spot.setPiece(new Piece());
+        return spot;
+    }
+
+    boolean getCorrectColor(int i, int j){
+        return (i + j)% 2 == 1;
     }
 }
--- src/Spot.java
+++ src/Spot.java
@@ -2,9 +2,14 @@
 
 public abstract class Spot {
     Pair<Integer, Integer> coord;
-    Piece piece;
+    Piece piece = null;
     // lets assume white is 0, and black is 1
     boolean color;
+
+    public Spot(Integer i, Integer j, boolean color){
+        this.coord = new Pair<>(i, j);
+        this.color = color;
+    }
 
     public Spot(Integer i, Integer j, Piece piece, boolean color){
         this.coord = new Pair<>(i, j);
@@ -22,26 +27,28 @@ public abstract class Spot {
 
     public Integer firstCoord(){ return coord.getFirst(); }
     public Integer secondCoord(){ return coord.getSecond(); }
+
+    public void setPiece(Piece piece){ this.piece = piece; }
 }
 
 class ChessSpot extends Spot {
-
-    public ChessSpot(Integer i, Integer j, Piece piece, boolean color) {
-        super(i, j, piece, color);
-    }
-
-    boolean checkCoord(Integer i){
-        System.out.println("Newem czy tu cos rzucac?");
-        return i <= 7 && i >= 0;
+    public ChessSpot(Integer i, Integer j, boolean color) {
+        super(i, j, color);
     }
 
     @Override
     public void setSpot(Spot spot){
         if(!checkCoord(spot.firstCoord()))
-            System.out.println("Jw, czy cos rzucac?");
+            System.out.println("Czy cos rzucac?");
         if(!checkCoord(spot.secondCoord()))
-            System.out.println("Jw, czy cos rzucac?");
+            System.out.println("Czy cos rzucac?");
 
         super.setSpot(spot);
+    }
+
+// helpers:
+    boolean checkCoord(Integer i){
+        System.out.println("Newem czy tu cos rzucac?");
+        return i <= 7 && i >= 0;
     }
 }
