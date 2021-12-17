bd418e88497d91105a29925b51f09a29f2c79ad5
Ryszard Pulawski <ryszap@gmail.com>
2021-12-17 10:13:32 +0100

    Board + Spot architecture + ugly Pair impl

--- src/Board.java
+++ src/Board.java
@@ -13,20 +13,43 @@ class ChessBoard extends Board{
     public void setupBoard() {
         for(int i = 0; i<8; i++){
             for(int j=0; j<8; j++)
-                board.get(i).get(j).setSpot(getCorrectSpotInit(i, j));
+                board.get(i).get(j).setSpot(getCorrectSpotChessInit(i, j));
         }
     }
 
 // helpers
-    Spot getCorrectSpotInit(int i, int j){
+    Spot getCorrectSpotChessInit(int i, int j){
         boolean color = getCorrectColor(i, j);
         Spot spot = new ChessSpot(i, j, color);
-        // TODO: madrze zakodzic to, jaka figurke zwraca, czy zostaje null
-        spot.setPiece(new Piece());
+
+        spot.setPiece(selectPiece(i, j));
         return spot;
     }
 
     boolean getCorrectColor(int i, int j){
         return (i + j)% 2 == 1;
+    }
+
+    Piece selectPiece(int i, int j){
+        if(i == 1 || i == 6)
+            return new Pawn();
+
+        if(i == 0 || i == 7){
+            if(j == 0 || j == 7)
+                return new Rook();
+
+            if(j == 1 || j == 6)
+                return new Knight();
+
+            if(j == 2 || j == 5)
+                return new Bishop();
+
+            if(j == 3)
+                return new Queen();
+
+            if(j == 4)
+                return new King();
+        }
+        return null;
     }
 }
