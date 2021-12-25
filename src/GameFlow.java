b87e1236961cb6ec6c2a15c57264d391807249a1
hannanowak <hanna.nowak@student.uj.edu.pl>
2021-12-25 12:41:03 +0100

    idk czy ma sens

--- src/GameFlow.java
+++ src/GameFlow.java
@@ -1,2 +1,52 @@
 public class GameFlow {
+
+    Player player0, player1;
+    boolean turn; //false - player0; true - player1
+
+    boolean areWeStillPlaying = true;
+
+// jak imie przekazywaź hmhmhhmhmh- chyba jednak nie?
+
+
+    public void decideGameType(boolean gameMode){
+        if(!gameMode){  // gramy z komputrem
+            humanVsComputer();
+        }else{  //gramy z ludziem
+            humanVsHuman();
+        }
+    }
+
+    public void humanVsComputer(){
+
+        player0 = new Player(); // człwoiek zaczyna deafultowo
+        player1 = new Player(player0);
+
+        turn = player0.colour;
+
+        while(areWeStillPlaying){
+
+        }
+
+    }
+    public void humanVsHuman(){
+
+        player0 = new Player();
+        player1 = new Player(player0);
+        player1.name = "guest";
+
+        turn = player0.colour;
+
+        while(areWeStillPlaying){
+
+        }
+
+
+    }
+
+
+
+
+
+
+
 }
--- src/Player.java
+++ src/Player.java
@@ -1,2 +1,21 @@
 public class Player {
+
+    boolean colour;
+    String name;
+
+    //kolor samemu się wybiera czy dogórnie? jak samemu to jak podać booleana?
+
+    //narazie jak się jest pierwszym graczem to się jest białymi później można wymyślić jak przekazyć?
+
+    Player(){
+        this.name = "human";
+        this.colour = false;
+    }
+
+    //jak gramy z komputerem to sami sobie wybieramy kolor a on dodstaje ten drugi?
+    Player(Player player){
+        this.name = "";
+        this.colour = !player.colour;
+    }
+
 }
